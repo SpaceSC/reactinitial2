72badbd64a78c96412e1a665363d0e9b38311535
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-24 14:21:15 +0100

    Render clients

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState} from "react";
+
 
 function Client({client}) {
-
-  
-
+  const [ show, setShow ] = useState(true);
 
   return (
     <div>
       <p>{client.name}</p>
+      {show && <button onClick={() => {setShow(false)}}>vaccinated</button>}
+      {!show && <p>{client.isVaccinated}</p>}
     </div>
   )
 }
