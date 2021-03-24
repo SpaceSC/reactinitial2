1801d9d77add5538c914d33e0576edca322dbe28
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-24 12:08:06 +0100

    Enable Search button if the input is >= 3 characters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css'
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+
+  const [ valid, setValid ] = useState(false);
+
+  function validate(e) {
+    if(e.target.value.length >= 3){
+      setValid(true);
+    }else{
+      setValid(false);
+    }
+  }
 
   return (
     <div className="App">
       <h1>Veterinarian admin - clients</h1>
+      <input type="text" id="text" name="text" onInput={validate}/>
+        {valid ? <button>Search</button> : <button disabled>Search</button>}
+
     </div>
   )
 }
